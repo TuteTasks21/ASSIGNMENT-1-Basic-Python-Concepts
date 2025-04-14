84eec7123068ee589973ceb0965ad099e59a520b
TuteTasks21 <apspssatin021@gmail.com>
2025-04-14 11:39:24 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number : "))
+b=int(input("Enter the second number : "))
+
+if b==0:
+	print("Please enter second number>0!!")
+else:
+    print("Addition: ",a+b)
+    print("Subtraction: ",a-b)
+    print("Multiplication: ",a*b)
+    print("Division: ",a/b)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+#Task2
+a=input("Enter your First name : ")
+b=input("Enter your last name : ")
+c=a+" "+b
+print(f"Hello, {c}! Welcode to the Python program.")
